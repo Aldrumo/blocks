0af839f50124da2bf83aa4371f4e759e6ef3fcb2
Mike Barlow <mike@mikebarlow.co.uk>
2020-12-24 17:31:50 +0000

    wip

--- src/Concerns/HasBlocks.php
+++ src/Concerns/HasBlocks.php
@@ -4,14 +4,32 @@ namespace Aldrumo\Blocks\Concerns;
 
 use Aldrumo\Blocks\Models\Block;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Collection;
 
 trait HasBlocks
 {
     public function blocks(): MorphMany
     {
-        return $this->morphyMany(
+        return $this->morphMany(
             Block::class,
             'item'
         );
+    }
+
+    public function saveBlocks(Collection $blocks)
+    {
+        $this->blocks()->delete();
+
+        $toSave = $blocks->map(
+            function ($block) {
+                return new Block([
+                    'slug' => $block['slug'],
+                    'content' => $block['content'],
+                ]);
+            }
+        );
+
+        $this->blocks()
+            ->saveMany($toSave);
     }
 }
